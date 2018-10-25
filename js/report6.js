107b1ab71800bd6292a788e9eef4463f6e8c915b
oxygener <YiTungTech@gmail.com>
2018-10-25 12:32:47 +0800

    modify: 調整Android下載格式

--- js/report6.js
+++ js/report6.js
@@ -344,13 +344,13 @@ function initDownloadButton() {
 
         var image_quality = 1.0;
         if (isAndroid()) {
-            image_quality = 0.5;
+            image_quality = 0.2;
         }
 
         html2canvas(document.getElementById("capture")).then(function(canvas) {
             var link = document.createElement('a');
             link.download = '優氧循環檢驗報告.jpg';
-            link.href = canvas.toDataURL("image/jpg",0.5);
+            link.href = canvas.toDataURL("image/jpg",image_quality);
             link.click();
         });
 
