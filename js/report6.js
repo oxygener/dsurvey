73db460f35b1217b0adc2e20be5ab4767b8991a4
oxygener <service@oxygener.com>
2019-10-29 00:57:32 +0800

    report6.js loadingImg fadeOut

--- js/report6.js
+++ js/report6.js
@@ -19,12 +19,12 @@ $(function() {
 
     function initLoadingAnimation(){
         console.log('initLoadingImage()');
-        // $("#report").hide();
+        $("#report").hide();
         $('#loadingimg').imgLoad(function(){
             // 圖片讀取完成
             setTimeout(function() {
                 $( "#loading" ).fadeOut( "1000", function() {//loading頁 fade out
-                    // $( "#report" ).fadeIn( "100", function() {});//report頁 fade in
+                    $( "#report" ).fadeIn( "100", function() {});//report頁 fade in
                 });
             }, 2700);
         });
